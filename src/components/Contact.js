f1f753a7d79172b103c03d5f88c265c29de90f7f
Jatin Varlyani <varlyanijatin88@gmail.com>
2018-12-15 21:44:08 +0530

    Added the state change in contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,16 +3,30 @@ import PropTypes from 'prop-types';
 
 
 class Contact extends Component {
-  render() {
+    
+    state  = {
+        showContactInfo : true
+    };
+
+    render() {
     
     const {name,email,phone} = this.props.contact; //Since we r getting contact from contact.js
+    const { showContactInfo} = this.state;
     return (
       <div className = "card card-body mb-3">
-        <h4>{name}</h4>
-        <ul className="list-group">
+        <h4>{ name }{' '}
+         <i onClick={() => 
+            this.setState({
+              showContactInfo : !this.state.showContactInfo
+            })
+         } 
+         className="fas fa-sort-down"/>
+        </h4>
+        {showContactInfo ? (<ul className="list-group">
             <li className="list-group-item">Email: {email}</li>
             <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        </ul>) : null} 
+        
       </div>
     )
   }
